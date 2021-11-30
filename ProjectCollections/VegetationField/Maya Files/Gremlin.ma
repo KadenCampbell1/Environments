e716ce3f7acd2a38e0210bc6b9369ebc62d875d5
Kaden(LenLap) <10836246@my.uvu.edu>
2021-11-29 21:26:28 -0700

    migrated vegetation field into repo

---
+++ ProjectCollections/VegetationField/Maya Files/Gremlin.ma
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb4605f1a4758e5f4abae819cb3f9e7dc9d99dd1dfb4437805255755121ab294
+size 787167
---
+++ ProjectCollections/VegetationField/Unity Files/VegetationField/Assets/_Scripts/Behaviours/ReturnToPositionBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class ReturnToPositionBehaviour : MonoBehaviour
+{
+    public float speed, percentComplete;
+    private float startTime;
+    private Vector3 startPosition, endPosition;
+
+    private WaitForFixedUpdate wffu;
+
+    private void Start()
+    {
+        wffu = new WaitForFixedUpdate();
+        startPosition = gameObject.transform.position;
+        endPosition = gameObject.transform.position;
+    }
+
+    private IEnumerator ReturnToStartPosition()
+    {
+        
+        if (percentComplete >= 1)
+        {
+            percentComplete = 1;
+        }
+    
+        while (percentComplete <= 1)
+        {
+            percentComplete += speed * Time.deltaTime;
+            transform.position = Vector3.Lerp(endPosition, startPosition, percentComplete);
+            // transform.rotation = Quaternion.Lerp(endPosition.rotation, startPosition.rotation, percentComplete);
+            yield return wffu;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        percentComplete = 0;
+        endPosition = gameObject.transform.position;
+        StartCoroutine(ReturnToStartPosition());
+    }
+}
